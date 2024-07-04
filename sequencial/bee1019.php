0d9f34be741eb2390ef18f47bab5a789212ab2c0
Miriam Batista <dev.miriambatista@gmail.com>
2024-07-04 11:54:42 -0300

    04/07

--- sequencial/bee1019.php
+++ sequencial/bee1019.php
@@ -1,12 +1,12 @@
 <?php
 
-$tempo_segundos =(int) readline("Tempo em segundos:   ");
+$tempo_segundos = (int) readline("Tempo em segundos:   ");
 
-$horas = (int)($tempo_segundos /3600);
+$horas = (int) ($tempo_segundos / 3600);
 
-$resto =$tempo_segundos % 3600;
+$resto = $tempo_segundos % 3600;
 
-$min = (int)($resto /60);
+$min = (int) ($resto / 60);
 
 $seg = $resto % 60;
 
