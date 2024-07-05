bdefacffa00c28c846714b43054ab27f9b7a2d4b
Miriam Batista <dev.miriambatista@gmail.com>
2024-07-05 11:43:37 -0300

    bee1035

---
+++ condicional/bee1044.php
@@ -0,0 +1,12 @@
+<?php
+
+$valores = explode(" ",readline("Digite 2 números: "));
+
+$n1 = (int) $valores[0];
+$n2 = (int) $valores[1];
+
+if (($n1 % $n2 == 0) || ($n2 % $n1 == 0)) {
+    echo "São Multiplos\n";
+} else {
+    echo "Não são multiplos\n";
+}
