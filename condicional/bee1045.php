110db7057377cc0834cd92747d74f488bd63f2e9
Miriam Batista <dev.miriambatista@gmail.com>
2024-07-09 08:17:39 -0300

    09/07

---
+++ condicional/bee1045.php
@@ -0,0 +1,45 @@
+<?php
+
+$valores = explode(" ",readline(" digite o valor: "));
+
+rsort($valores);
+$a = $valores[0];
+$b = $valores[1];
+$c = $valores[2];
+
+
+// if( $valores [0] > $valores[1] && $valores[0] > $valores [2]){
+//     $maior = $valores[0];
+//     $meio = $valores[1];
+//     $menor = $valores[2];
+
+// } else if ($valores[1] > $valores[0] && $valores[1] > $valores[2]){
+//     $maior = $valores[1];
+//     $meio = $valores[0];
+//     $menor = $valores[2];
+// }else{
+//     $maior = $valores[2];
+//     $meio = $valores[0];
+//     $menor = $valores[1];
+//}
+
+//$a =$maior; $b = $meio; $c = $menor;
+
+if($a >= $b + $c){
+    echo "NAO FORMA TRIANGULO". PHP_EOL;
+} else{
+    if (pow($a,2) == pow($b,2) + pow($c,2)){
+    echo " TRIANGULO RETANGULO". PHP_EOL;
+        }else if (pow($a,2) > pow($b,2) + pow($c,2)){
+        echo " TRIANGULO OBTUSANGULO". PHP_EOL;
+        }else if (pow($a,2) < (pow($b,2)+pow($c,2))){
+
+        }
+        
+        echo "TRIANGULO ACUTANGULO". PHP_EOL;
+
+        }if ($a == $b && $b == $c){
+            echo " TRIANGULO EQUILATERO". PHP_EOL;
+        }else if ($a == $b && $a != $c || $a == $c && $a!= $b || $b == $c && $b!=$a){
+            echo "TRIANGULO ISOSCELES". PHP_EOL;
+        }
