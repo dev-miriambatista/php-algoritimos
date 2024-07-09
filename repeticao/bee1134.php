fc8ce7c15ac97f39a350e519735c6242819a93fa
Miriam Batista <dev.miriambatista@gmail.com>
2024-07-09 11:50:53 -0300

    bee1134

--- repeticao/bee1134.php
+++ repeticao/bee1134.php
@@ -1,4 +1,23 @@
 <?php
 
 $cod = explode(" ", (readline ("Qual combustível? ")));
+while ($cod !=4){
+    swith ($cod) {
+        case 1:
+            $alcool++;
+            break;
+            case2:
+                $alcool++;
+                break;
+                case3:
+                    $alcool++;
+                    break;
+    }
+    $cod = (int) readline ("Código do combustível: ");
+
+    echo "MUITO OBRIGADO". PHP_EOL;
+    echo "Alcool: $alcool" . PHP_EOL;
+    echo "Gasolina: $gasolina" . PHP_EOL;
+    echo "Diesel: $diesel" . PHP_EOL;
+}
 
