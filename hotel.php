375db4ca6910864dbd7d53029352285fd1ab97f8
Miriam Batista <dev.miriambatista@gmail.com>
2024-07-10 11:53:17 -0300

    10/07

---
+++ hotel.php
@@ -0,0 +1,11 @@
+<?php
+
+//Ocupação dos Hotéis : Escreva um programa que comece com a capacidade total de um hotel em Marica e reduza esse 
+//número à medida que os quartos são reservados (pergunte ao usuário quantos quartos foram reservados cada dia). 
+//Continue até que o hotel esteja completamente reservado, e para cada entrada, informar quantos quartos ainda estão disponíveis.
+
+$capacidade = (int) readline ("Capacidade em qauntos do hotel: ");
+
+for($i = $capacidade; $i > 0; $i--){
+    
+}
